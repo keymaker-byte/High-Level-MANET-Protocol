7de97c39b9568eae944a57d0fb807c1fd03e76dd
Juan Rodriguez <zenilt@gmail.com>
2011-08-06 22:21:59 -0400

    directory arrangement

---
+++ source/windowsMobile/CommControl/ConfigurationControl.cs
@@ -0,0 +1,108 @@
+/***************************************************************************
+----------------------------------------------------------------------------
+  This file is part of the HLMP API - Plug-ins Source Code.
+  http://hlmprotocol.bicubic.cl
+ 
+  Copyright (C) Bicubic TMG.  All rights reserved.
+ 
+  This source code is intended only as a supplement to HLMP API 
+  and/or on-line documentation.  
+ 
+  THE SOURCE CODE CONTAINED WITHIN THIS FILE AND ALL RELATED      
+  FILES OR ANY PORTION OF ITS CONTENTS SHALL AT NO TIME BE        
+  COPIED, TRANSFERRED, SOLD, DISTRIBUTED, OR OTHERWISE MADE       
+  AVAILABLE TO OTHER INDIVIDUALS WITHOUT EXPRESS WRITTEN CONSENT  
+  AND PERMISSION FROM BICUBIC TMG.   
+
+  THIS CODE AND INFORMATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY
+  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+  PARTICULAR PURPOSE.
+----------------------------------------------------------------------------
+****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using NetLayer;
+using CommLayer;
+using System.Net;
+using System.IO;
+
+namespace CommControl
+{
+    /// <summary>
+    /// Control gráfico para el ingreso de datos de configuración (formulario)
+    /// </summary>
+    public partial class ConfigurationControl : UserControl
+    {
+        /// <summary>
+        /// Los datos de configuración
+        /// </summary>
+        private Configuration _configurationData;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public ConfigurationControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Los datos de configuración editados en el formulario (null si se ha cancelado).
+        /// </summary>
+        public Configuration ConfigurationData
+        {
+            get { return _configurationData; }
+            set { _configurationData = value;  }
+        }
+
+        /// <summary>
+        /// Inicializa los datos del formulario con la información de la propiedad ConfigurationData
+        /// </summary>
+        public void init()
+        {
+            //Crea la nueva clase de configuracion, si no existe
+            if (ConfigurationData == null)
+            {
+                ConfigurationData = new Configuration();
+            }
+
+            //inicializa los formularios
+            textBoxName.Text = ConfigurationData.NetUser.Name;
+        }
+
+        /// <summary>
+        /// Guarda la información actual del formulario en la propiedad ConfigurationData
+        /// </summary>
+        /// <returns>true si el formulario ha sido llenado correctamente y ha sido guardado, false si no
+        /// Si no ha sido bien llenado, se emite una ventana modal con el mensaje correspondiente</returns>
+        public bool aceptar()
+        {
+            //Nombre
+            if (textBoxName.Text != "")
+            {
+                ConfigurationData.NetUser.Name = textBoxName.Text;
+            }
+            else
+            {
+                MessageBox.Show("Debe especificar un Nombre de Usuario válido.");
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Cancela los cambios hechos a la configuración, la propiedad ConfigurationData se vuelve null
+        /// </summary>
+        public void cancelar()
+        {
+            ConfigurationData = null;
+        }
+    }
+}
---
+++ source/windowsMobile/CommControl/StatusControl.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CommControl
+{
+    partial class StatusControl
+    {
+        /// <summary> 
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar 
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StatusControl));
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.imageList = new System.Windows.Forms.ImageList();
+            this.SuspendLayout();
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonDisconnect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonDisconnect.Enabled = false;
+            this.buttonDisconnect.Location = new System.Drawing.Point(130, 131);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(96, 23);
+            this.buttonDisconnect.TabIndex = 7;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelStatus.Location = new System.Drawing.Point(56, 106);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(143, 15);
+            this.labelStatus.Text = "Desconectado";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonConnect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonConnect.Location = new System.Drawing.Point(28, 131);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(96, 23);
+            this.buttonConnect.TabIndex = 6;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox.Location = new System.Drawing.Point(112, 66);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(32, 32);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageSize = new System.Drawing.Size(64, 64);
+            this.imageList.Images.Clear();
+            this.imageList.Images.Add(((System.Drawing.Image)(resources.GetObject("resource"))));
+            this.imageList.Images.Add(((System.Drawing.Image)(resources.GetObject("resource1"))));
+            this.imageList.Images.Add(((System.Drawing.Image)(resources.GetObject("resource2"))));
+            // 
+            // StatusControlCompact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "StatusControlCompact";
+            this.Size = new System.Drawing.Size(256, 217);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.ImageList imageList;
+    }
+}
